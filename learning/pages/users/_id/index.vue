5427e28e8988c8e2b78243dd510842b59e62df92
Tatsuro <contacts@tatsuro.dev>
2019-06-06 12:10:33 +0900

    ルートパラメーターにバリデーション追加 12

--- learning/pages/users/_id/index.vue
+++ learning/pages/users/_id/index.vue
@@ -4,3 +4,17 @@
     <h2>This is user's id {{ $route.params.id }}</h2>
   </div>
 </template>
+<script>
+export default {
+  /*
+  validate(context) {
+    // 正規表現.testメソッドでバリデーション対象を指定
+    return /^\d+$/.test(context.params.id);
+  }
+  */
+  // オブジェクトの分割代入、Destructuring assignment
+  validate({ params }) {
+    return /^\d+$/.test(params.id);
+  }
+};
+</script>
