60aca50776bec0a16d25179bf4a395df2b7c5742
Tatsuro <contacts@tatsuro.dev>
2019-06-06 12:45:19 +0900

    Nav component作成 15

--- learning/layouts/default.vue
+++ learning/layouts/default.vue
@@ -1,8 +1,19 @@
 <template>
   <div>
+    <Nav/>
     <nuxt/>
   </div>
 </template>
+<script>
+import Nav from "@/components/Nav";
+
+export default {
+  components: {
+    Nav
+  }
+};
+</script>
+
 <style>
 body {
   background-color: azure;
