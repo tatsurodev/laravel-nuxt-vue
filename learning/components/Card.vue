d9fea42bebffbc2a1dd3ee1f38e74c321ee7dd49
Tatsuro <contacts@tatsuro.dev>
2019-06-06 18:34:25 +0900

    nuxt linkで個別投稿ページへのリンク作成 26

--- learning/components/Card.vue
+++ learning/components/Card.vue
@@ -3,7 +3,10 @@
     <div class="card bg-light mb-3" style="max-width: 18rem;">
       <div class="card-header">Post: {{ post.id }}</div>
       <div class="card-body">
-        <h5 class="card-title">{{ post.title }}</h5>
+        <h5 class="card-title">
+          <!-- ルート名は、フォルダ名-パラメータ -->
+          <nuxt-link :to="{ name: 'posts-id', params: { id: post.id }}">{{ post.title }}</nuxt-link>
+        </h5>
       </div>
     </div>
   </div>
