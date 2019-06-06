3c982d2aa6ae7dd456159633b9992bf1e77aad18
Tatsuro <contacts@tatsuro.dev>
2019-06-06 12:44:30 +0900

    usersのchild component削除

--- learning/pages/users.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div style="background: red;">
-    <h4>You can see me in all users related pages.</h4>
-    <!-- このファイル名(users)と同じフォルダ名のページがnext-childタグに挿入される -->
-    <nuxt-child></nuxt-child>
-  </div>
-</template>
