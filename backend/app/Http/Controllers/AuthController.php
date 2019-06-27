310680ed371ebb0b04183aaa3c0c69fd874d6e0a
Tatsuro <contacts@tatsuro.dev>
2019-06-27 20:35:19 +0900

    AuthController@login作成 51

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\User;
 use Illuminate\Http\Request;
 use App\Http\Requests\UserRegisterRequest;
+use App\Http\Requests\UserLoginRequest;
 use App\Http\Resources\User as UserResource;
 
 class AuthController extends Controller
@@ -23,6 +24,22 @@ class AuthController extends Controller
             return abort(401);
         }
         // resourceにtoken追加
+        return (new UserResource($request->user()))->additional([
+            'meta' => [
+                'token' => $token,
+            ],
+        ]);
+    }
+
+    public function login(UserLoginRequest $request)
+    {
+        if (!$token = auth()->attempt($request->only(['email', 'password']))) {
+            return response()->json([
+                'errors' => [
+                    'email' => ['Sorry we cannot find you with those details.']
+                ]
+            ], 422);
+        }
         return (new UserResource($request->user()))->additional([
             'meta' => [
                 'token' => $token,
