1ef551e5f6ee578f9051c84fe381a25d028c1589
Tatsuro <contacts@tatsuro.dev>
2019-07-05 04:00:24 +0900

    Navbar componentにlogout機能追加 60

--- frontend/components/Navbar.vue
+++ frontend/components/Navbar.vue
@@ -38,7 +38,7 @@
             <a class="nav-link">{{ user.name }}</a>
           </li>
           <li class="nav-item">
-            <nuxt-link to="/register" class="nav-link">Logout</nuxt-link>
+            <a @click.prevent="logout()" class="nav-link">Logout</a>
           </li>
         </ul>
       </template>
@@ -46,5 +46,11 @@
   </nav>
 </template>
 <script>
-export default {};
+export default {
+  methods: {
+    logout() {
+      this.$auth.logout();
+    }
+  }
+};
 </script>
